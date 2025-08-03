b879ef60cd70344690c3630b4dce32ff23f7d452
Alenia24 <alenia.allenn@gmail.com>
2025-08-03 02:53:09 -0400

    Import task controller

--- server/routes/task/task-route.js
+++ server/routes/task/task-route.js
@@ -3,23 +3,24 @@ import authenticateToken from "../../middleware/auth-middleware";
 const router = express.Router();
 
 // Import task controller
+import * as TaskController from "../../controllers/task/task-controller.js"
 
 // Protect all routes
 router.use(authenticateToken)
 
 // Create a task
-router.post("/",)
+router.post("/", TaskController.createTask)
 
 // Get all tasks
-router.get("/",)
+router.get("/", TaskController.getTasks)
 
 // Get a task by id
-router.get("/:id",)
+router.get("/:id", TaskController.getTask)
 
 // Update a task
-router.post("/:id",)
+router.post("/:id", TaskController.updateTask)
 
 // Delete a task
-router.delete("/:id",)
+router.delete("/:id", TaskController.deleteTask)
 
 export default router;
