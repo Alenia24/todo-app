fa8c9be0aafbe89e5045c2aa852b0067f3065f55
Alenia24 <alenia.allenn@gmail.com>
2025-08-03 00:33:31 -0400

    Create auth route

---
+++ server/routes/auth/auth-route.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+
+// // Import auth controller
+// import * as AuthController
+
+// Create a user
+router.post("/register");
+
+// Login a user
+router.post("/login");
+
+export default router;
