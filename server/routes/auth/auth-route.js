9649cd40f2ec67ae17b7d6fc1e556016a7355bbf
Alenia24 <alenia.allenn@gmail.com>
2025-08-03 01:15:24 -0400

    Import auth controller

--- server/routes/auth/auth-route.js
+++ server/routes/auth/auth-route.js
@@ -2,12 +2,12 @@ import express from "express";
 const router = express.Router();
 
 // // Import auth controller
-// import * as AuthController
+import * as AuthController from "../../controllers/auth/auth-controller.js";
 
 // Create a user
-router.post("/register");
+router.post("/register", AuthController.registerUser);
 
 // Login a user
-router.post("/login");
+router.post("/login", AuthController.loginUser);
 
 export default router;
