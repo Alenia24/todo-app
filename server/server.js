9771dda626c573cb9daa833f0fb7db25b885c88d
Alenia24 <alenia.allenn@gmail.com>
2025-08-03 01:22:59 -0400

    Import routers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
-import express from "express"
-const app = express()
+import express from "express";
+const app = express();
+import { config } from "dotenv";
+config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 // Import the databse connection
-import "./config/db.js"
+import "./config/db.js";
+
+// Import routes
+import authRouter from "./routes/auth/auth-route.js";
+
+// Enable to use req.body when forms are submitted
+app.use(express.urlencoded({ extended: true }));
+// Enable to use req.body without a form, eg. from req.bin, postman, etc
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to todo app API.")
-})
+  res.send("Welcome to todo app API.");
+});
+
+// Enable routes to use
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`)
-})
+  console.log(`Server running on port: ${PORT}`);
+});
