c1ecf79e45c1789caedbe45dd4a17e1db63dbe67
Alenia24 <alenia.allenn@gmail.com>
2025-08-08 20:20:15 -0400

    Create homepage route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Homepage from "./pages/Homepage/Homepage.jsx";
 
 function App() {
-  return (
-    <>
-      
-    </>
-  )
+  return <div className="App">
+    <Routes>
+      <Route path="/" element={<Homepage/>} />
+    </Routes>
+  </div>;
 }
 
-export default App
+export default App;
