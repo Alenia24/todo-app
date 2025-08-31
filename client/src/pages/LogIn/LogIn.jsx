2a702ce6584e5da82bbc54bae45b27451163824b
Alenia24 <alenia.allenn@gmail.com>
2025-08-30 21:32:08 -0400

    Update links and button

--- client/src/pages/LogIn/LogIn.jsx
+++ client/src/pages/LogIn/LogIn.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 import Logo from "../../assets/logo.png";
 import LoginImage from "../../assets/login.png";
@@ -6,25 +7,27 @@ import "./LogIn.css"
 
 export default function LogIn() {
   return (
-    <div className='login-container'>
-            <div className='left'>
-                <img className='img-fluid' src={LoginImage} alt="" />
-            </div>
-            <div className="right">
-                <img src={Logo} alt="" width={70}/>
-                <h1>Get Started</h1>
-                <h5 className='welcome'>Welcome Back to Mintra!</h5>
-              <form className='login-form'>    
-                <label> Email</label> <br />
-                <input type="email"  /> <br />
-    
-                <label>Password</label> <br />
-                <input type="password" name="password" id="password" /> <br />
-    
-                <input type="submit" value="Log In" />
-              </form>
-              <p className='account'>Don't have an account? <a href="">Sign Up</a></p>
-            </div>
-        </div>
-  )
+    <div className="login-container">
+      <div className="left">
+        <img className="img-fluid" src={LoginImage} alt="" />
+      </div>
+      <div className="right">
+        <Link to="/">
+          <img src={Logo} alt="" width={70} />
+        </Link>
+        <h1>Get Started</h1>
+        <h5 className="welcome">Welcome Back to Mintra!</h5>
+        <form className="login-form">
+          <label> Email</label> <br />
+          <input type="email" /> <br />
+          <label>Password</label> <br />
+          <input type="password" name="password" id="password" /> <br />
+          <input type="submit" value="Log In" />
+        </form>
+        <p className="account">
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
