a6a9ebb27ef5d0c52a2323c26ff2d2c2199eb93d
Alenia24 <alenia.allenn@gmail.com>
2025-08-30 20:46:34 -0400

    Design signup page

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
+import Logo from "../../assets/logo.png"
+import LoginImage from "../../assets/login.png"
 import "./SignUp.css"
+import Navigation from '../../components/Navigation/Navigation.jsx'
+import Footer from "../../components/Footer/Footer.jsx"
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className='signup-container'>
+        <div className='left'>
+            <img className='img-fluid' src={LoginImage} alt="" />
+        </div>
+        <div className="right">
+            <img src={Logo} alt="" width={70}/>
+            <h1>Get Started</h1>
+            <h5 className='welcome'>Welcome to Mintra! Let's create your account.</h5>
+          <form className='signup-form'>
+            <label> Username</label> <br />
+            <input type="text"  /> <br />
+
+            <label> Email</label> <br />
+            <input type="email"  /> <br />
+
+            <label>Password (6 or more characters)</label> <br />
+            <input type="password" name="password" id="password" /> <br />
+
+            <label>Confirm Password</label> <br />
+            <input type="password" name="confirm-password" id="confirm-password" />
+            <br />
+            <input type="submit" value="Sign Up" />
+          </form>
+          <p className='account'>Already have an account? <a href="">Log in</a></p>
+        </div>
+    </div>
+  );
 }
