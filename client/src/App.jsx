170940fcf8194063e5637b4375fe693e65369d47
Alenia24 <alenia.allenn@gmail.com>
2025-08-30 21:03:18 -0400

    Import login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Homepage from "./pages/Homepage/Homepage.jsx";
 import SignUp from "./pages/SignUp/SignUp.jsx";
+import LogIn from "./pages/LogIn/LogIn.jsx";
 
 function App() {
-  return <div className="App">
-    <Routes>
-      <Route path="/" element={<Homepage/>} />
-      <Route path="/signup" element={<SignUp/>} />
-    </Routes>
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn/>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
